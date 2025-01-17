521ae28b705b22f6f12c8d2080b44d4a1cef6a0d
SAKTHIVAISHNAVI N <sakthivaishnavi12@gmail.com>
2025-01-17 18:46:26 +0530

    initialize frontend with React, Vite, and Tailwind CSS setup

---
+++ frontend/src/Components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Job Portal</h1>
+      <p>Welcome to the Job Portal</p>
+      <button>Signup</button>
+      <button>Login</button>
+      
+
+
+
+    </div>
+  )
+}
+
+export default Home
